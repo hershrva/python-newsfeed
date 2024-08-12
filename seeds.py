e10fef5c1e58ff880d7b0ba881781c3782422da1
hershRVA <herchenrother@gmail.com>
2024-08-12 14:18:56 -0400

    change to seeds

--- seeds.py
+++ seeds.py
@@ -21,22 +21,22 @@ db.commit()
 
 # insert posts
 db.add_all([
-  Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
-  Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
-  Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
-  Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
-  Post(title='Pellentesque eget nunc', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
+  Post(title='Sofware Development Times', post_url='https://sdtimes.com/', user_id=1),
+  Post(title='Developer Tech News', post_url='https://www.developer-tech.com/', user_id=1),
+  Post(title='InfoQ', post_url='https://www.infoq.com/', user_id=2),
+  Post(title='The New Stack', post_url='https://thenewstack.io/software-development/', user_id=3),
+  Post(title='Wired', post_url='https://www.wired.com/tag/developers/', user_id=4)
 ])
 
 db.commit()
 
 # insert comments
 db.add_all([
-  Comment(comment_text='Nunc rhoncus dui vel sem.', user_id=1, post_id=2),
-  Comment(comment_text='Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', user_id=1, post_id=3),
-  Comment(comment_text='Aliquam erat volutpat. In congue.', user_id=2, post_id=1),
-  Comment(comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', user_id=2, post_id=3),
-  Comment(comment_text='In hac habitasse platea dictumst.', user_id=3, post_id=3)
+  Comment(comment_text='Great Post!', user_id=1, post_id=2),
+  Comment(comment_text='Really interesting site.  Thanks for contributing!', user_id=1, post_id=3),
+  Comment(comment_text='This really helped me further my knowledge', user_id=2, post_id=1),
+  Comment(comment_text='Awesome articles.  Cant wait to read more', user_id=2, post_id=3),
+  Comment(comment_text='LOVE IT!', user_id=3, post_id=3)
 ])
 
 db.commit()
